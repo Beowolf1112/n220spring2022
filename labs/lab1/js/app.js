e1fbad301afcb7a9ffbd9345fcd8157570f8674e
Beowolf1112 <97626771+Beowolf1112@users.noreply.github.com>
2022-01-18 10:52:46 -0500

    Class Examples 1/18

--- labs/lab1/js/app.js
+++ labs/lab1/js/app.js
@@ -1,7 +1,19 @@
 function setup() {
     createCanvas(800,600);
-}
 
-function draw() {
-    circle(mouseX,mouseY, frameCount/30);
+    stroke(255, 255, 255)
+    strokeWeight(10);
+    
+    fill(104, 103, 172); /*#6877AC*/
+    circle(150, 150, 200);
+    fill(162, 103, 172); /*A267AC*/
+    circle(150, 150, 150);
+    fill(206, 123, 176); /*CE7BB0*/
+    circle(150, 150, 50);
+    fill(255, 188, 209); /*FFBCD1*/ 
+   
+    circle(150, 150, 40);
+    circle(300, 150, 40);
+
+    arc(200, 300, 200, 200, 0, PI + QUARTER_PI, CHORD);
 }
