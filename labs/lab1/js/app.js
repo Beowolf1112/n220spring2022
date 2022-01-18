23e0f9db65a798914371dbdd5041dbdd6da60ca9
Beowolf1112 <97626771+Beowolf1112@users.noreply.github.com>
2022-01-18 11:22:21 -0500

    Class Examples 1/18 pt.2

--- labs/lab1/js/app.js
+++ labs/lab1/js/app.js
@@ -1,19 +1,45 @@
-function setup() {
-    createCanvas(800,600);
+//Jack Goodman
+//1/18/2022
+//n220
 
+//application level variables
+let size = 10;
+
+//setup the application
+
+function setup() {
+    //create canvas
+    createCanvas(800,600);
+    /*
     stroke(255, 255, 255)
     strokeWeight(10);
     
-    fill(104, 103, 172); /*#6877AC*/
+    fill(104, 103, 172);
     circle(150, 150, 200);
-    fill(162, 103, 172); /*A267AC*/
+    fill(162, 103, 172);
     circle(150, 150, 150);
-    fill(206, 123, 176); /*CE7BB0*/
+    fill(206, 123, 176);
     circle(150, 150, 50);
-    fill(255, 188, 209); /*FFBCD1*/ 
+    fill(255, 188, 209);
    
     circle(150, 150, 40);
     circle(300, 150, 40);
 
-    arc(200, 300, 200, 200, 0, PI + QUARTER_PI, CHORD);
+    arc(200, 300, 200, 200, 0, PI + QUARTER_PI, CHORD); */
+}
+
+function draw(); {
+
+    //increase size
+    size = size + 1;
+    console.log(size);
+
+    //change fill color to red
+    fill(255, 99, 99);
+    //draw a circle to the center at an increasing rate
+    circle(150, 150, size);
+
+    if(size > 200) {
+        fill(255, 99, 255);
+    }
 }
