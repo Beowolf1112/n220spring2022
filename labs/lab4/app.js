49b0788107bda2fa62963f32a25d4ad02b958182
Beowolf1112 <97626771+Beowolf1112@users.noreply.github.com>
2022-02-08 10:58:05 -0500

    in-class examples 2/8/22

---
+++ labs/lab4/app.js
@@ -0,0 +1,12 @@
+//Jack Goodman
+//2/1/2022
+//n220
+
+
+
+//in-class 2/8/22
+/*let dvTest = document.getElementById("dvTest");
+console.log(dvTest);
+
+dvTest.innerHTML = "Hello World";
+dvTest.style.backgroundColor = "#00FF99";*/
