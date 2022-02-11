663a0028de39fa9d1687b205f433a88ec7f0609e
Beowolf1112 <97626771+Beowolf1112@users.noreply.github.com>
2022-02-10 22:17:55 -0500

    Late Night 2/10/22

--- labs/lab4/app.js
+++ labs/lab4/app.js
@@ -2,7 +2,7 @@
 //2/1/2022
 //n220
 
-//Data-driven display w/ function
+//#1 Data-driven display w/ function
 /*creates object*/
 let objBall = {
     x: 12, y: 10,
@@ -14,18 +14,79 @@ console.log(objBall.y); //10
 objBall.y *= 12; //Increased Y
 console.log(objBall.y.toFixed(2));*/
 
+let objPet = {
+    x: 15, y: 15,
+    petColor: "#990000",
+    size: 15,
+    offsetX: 1,
+    // use offsetX for lab4
+}
+
+//this is #3's attempt for code to store objPet and its elements
+window.localStorage.setItem("objPet", JSON.stringify(objPet));
+
+let newPet = window.localStorage.getItem("objPet");
+console.log(JSON.parse(newPet));
+
+//now back to #1
 function setup() {
     createCanvas(600,400);
     background(200);
 }
 
+//Still a part of #1, but might also be used for #2 Virtual Pet?
 function draw() {
     background(200);
     
+    //make shape follow mouse
     fill(objBall.ballColor);
     rect(mouseX - objBall.width/2, mouseY - objBall.height/2, objBall.width, objBall.height);
-}
 
+    //make pet to follow mouse
+    fill(objPet.petColor);
+    circle(objPet.x, objPet.y, objPet.size);
+    //objPet.x += 1;
+
+    //make Pet change direction in reaction to mouse coordinates
+    if (mouseX > 300) {
+        objPet.x += 1;
+    }
+    if (mouseX < 300) {
+        objPet.x -=1;
+    }
+    if (mouseY > 200) {
+        objPet.y += 1;
+    }
+    if (mouseY < 200) {
+        objPet.y -=1;
+    }
+
+    //attempt at world boarder
+    if (objPet.x < 0) {
+        stop (objPet.x);
+        objPet.x -=2;
+    }
+    if (objPet.x > 600) {
+       stop(objPet.x);
+        objPet.x +=2;
+    }
+    if (objPet.y < 0) {
+        stop(objPet.y);
+        objPet.y -=2;
+    }
+    if (objPet.y > 400) {
+        stop(objPet.y);
+        objPet.y +=2;
+    }
+
+    let mousePressed = onclick;
+
+    if (mousePressed) {
+        var x = objPet.offsetX;
+
+    }
+    
+}
 
 //in-class 2/10/22
 /*let objTV = {
