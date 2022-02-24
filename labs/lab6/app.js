05ad8fc8f9bf70f0f4c5c24080473ad137b5c03e
Beowolf1112 <97626771+Beowolf1112@users.noreply.github.com>
2022-02-24 12:56:32 -0500

    in-lab 2/24/22

--- labs/lab6/app.js
+++ labs/lab6/app.js
@@ -6,3 +6,26 @@
 //Review - No New Code
 
 //in-class 2/24/22
+/*let testArray = [20, 45, 10, 70, 88, 100];
+let res = countTwenties(testArray);
+console.log("Final Count: " + res);
+//Takes an Array of numbers as an argument
+function countTwenties(numbersArray) {
+    let count = 0;
+    //counts numbers in array above 20
+    //console.log(testArray[0] > 20);
+    for(var i = 0; i < testArray.length; i++) {
+        if(numbersArray[i] > 20) {
+            console.log(numbersArray[i] + ": its greater than 20");
+            count = count = 1;
+        }
+    } 
+    //(if count is less than three): The count of those numbers
+    if(count < 3) {
+        return count;
+    }
+    //(if count is greater than three): the number -1
+    if(count > 3) {
+        return -1;
+    }
+}*/
