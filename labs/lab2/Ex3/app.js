b5c16423a5bed84ec528352218dff10d51c98727
Beowolf1112 <97626771+Beowolf1112@users.noreply.github.com>
2022-03-01 07:38:00 -0500

    mid morning update 3/1

--- labs/lab2/Ex3/app.js
+++ labs/lab2/Ex3/app.js
@@ -4,22 +4,21 @@
 
 //Ever Larger, until not
 let dia = {
-    x: 200,
-    y: 200,
+    x: 200, y: 200,
     color: "#990000",
     size: 1
 };
 
 function setup() {
-    createCanvas(400, 400);
-    background(62, 237, 108)
+    createCanvas(300, 300);
+    background(62, 237, 108);
 }
 
 function draw() {
-    dia.x = dia.x + 1;
-    if(dia.x > 200) dia = 0; {
-        //fill(dia.color); =/= acceptable
-        fill(0, 0, 0);
-        circle(12, 100, dia.size);
+    //fill(dia.color); =/= acceptable
+    circle(150, 150, dia.size);
+
+    dia.size = dia.size + 1;
+    if(dia.size > 200) dia.size = 1; {
     }
 }
