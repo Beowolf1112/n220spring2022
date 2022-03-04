6d88e1628755eb3b3fbb82ea9aa3ab566fe75082
Beowolf1112 <97626771+Beowolf1112@users.noreply.github.com>
2022-03-03 21:54:54 -0500

    pre-midnight update 3/3

--- labs/lab7/app.js
+++ labs/lab7/app.js
@@ -3,7 +3,30 @@
 //n220
 
 //UI Navigation Highlighter
+let one = document.getElementById("one");
+let two = document.getElementById("two");
+let three = document.getElementById("three");
+let four = document.getElementById("four");
+let five = document.getElementById("five");
+let six = document.getElementById("six");
 
+function over() { //Over & Out
+    let mouseX = MouseEvent //trigger change for mouse position
+    let numb = "mouseout";
+   
+     if(mouseX < 100) numb = "mouseover";
+    dvOne.innerHTML = numb;
+    if(numb = "mouseover") { 
+    }
+} function out() {
+    let mouseX = MouseEvent //trigger change for mouse position
+    let numb = "mouseover";
+    if(mouseX > 100) numb = "mouseout"; //change color when mouse out
+    dvOne.innerHTML = numb;
+    if(numb = "mouseout") {
+        dvOne.style.backgroundColor = "#0000ff";
+    }
+}
 
 //Rock, Paper, Scissors (Guard) 
 let playerScore = 0;
