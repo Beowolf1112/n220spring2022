795555447a5baf75de5f0c3b0d9608e877bd965d
Beowolf1112 <97626771+Beowolf1112@users.noreply.github.com>
2022-04-05 11:28:37 -0400

    in class 4/5

--- labs/lab9/app.js
+++ labs/lab9/app.js
@@ -2,3 +2,25 @@
 //4/5/2022
 //n220
 
+let exampleDiv = document.getElementById("example");
+exampleDiv.addEventListener("click", dothing);
+
+let exampleDiv2 = document.getElementById("example2");
+exampleDiv2.addEventListener("click", dothing2);
+
+function dothing() {
+    //console.log(event.target); //makes a refrence to the code in console log
+    event.target.innerHTML = "DONE"; //changes button text
+    //console.log("I've been invoked");
+}
+
+function dothing2() {
+    event.target.innerHTML = "DONE";
+}
+
+var grid = [
+    [0, 0, 0,],
+    [0, 0, 0],
+    [0, 0, 0]
+];
+
