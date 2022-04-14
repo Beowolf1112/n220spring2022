baaeb72301b9ddcef8d063cdd54ae06ea0216f13
Beowolf1112 <97626771+Beowolf1112@users.noreply.github.com>
2022-04-14 13:06:22 -0400

    in-lab update 4/14

--- labs/lab10/app.js
+++ labs/lab10/app.js
@@ -2,8 +2,46 @@
 //4/12/2022
 //n220
 
+//Check Win
+let win = document.getElementById("win");
+
+function winner() {
+    //let prize = win.value; //recognises individual letters/numbers
+    let prize = win.value.split(" ");
+    let newPrize = ""; //resets input
+
+    for(var i=0; i<prize.length; i++) {
+        if(prize[i]=="111") { //sets peramiters for win conditions
+            console.log("winner");
+        } else {
+            newPrize += prize[i];
+            console.log("not winner");
+        }
+    }
+}
+
+//Bad Word Catcher with Loops
+let curse = document.getElementById("curse");
+
+function sailor() {
+    let naughty = curse.value.split(" "); //recognises words, not individual letters/numbers
+    let newNaughty = "";
+
+    for(var i=0; i<naughty.length; i++) {
+        if(naughty[i]=="clear" ||naughty[i]=="water" || naughty[i]=="tires"
+        ) {
+            console.log("naughty words");
+        } else {
+            newNaughty += naughty[i];
+            console.log("no naughty words");
+        }
+    }
+}
+
+//
+
 //----------In-Class_4/12-------------------------------------------
-let allNamesClicked = "";
+/*let allNamesClicked = "";
 let dvSelectedCharacter = document.getElementById("selectedCharacter");
 let elements = document.getElementsByClassName("ex"); //acts as array below
 //let elements = [document.getElementById("example"), document.getElementById("example2")]; 
@@ -19,4 +57,13 @@ function dothing(event) {
     //Allows the elements of character name to remain on page and appear multiple times without reseting
     allNamesClicked += event.target.dataset.character;
     dvSelectedCharacter.innerHTML = allNamesClicked;
-}
+}*/
+//-----------------Old HTML-------------------------------------------
+/*  <div id="selectedCharacter"></div>
+    <button class="ex" data-character="Hulk">buttons</button>
+    <button class="ex" data-character="Spiderman">buttons</button>
+    <button class="ex" data-character="Miss Marvel">buttons</button>
+    <button class="ex" data-character="Iron Man">buttons</button>
+    <button class="ex" data-character="Hawkeye">buttons</button>
+    <button class="ex" data-character="Jessica Jones">buttons</button> 
+*/
