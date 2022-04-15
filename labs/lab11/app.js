310d502685833911d2e721a0b2ffa426bf36edf2
Beowolf1112 <97626771+Beowolf1112@users.noreply.github.com>
2022-04-15 04:51:39 -0400

    late night 4/15 pt.2

--- labs/lab11/app.js
+++ labs/lab11/app.js
@@ -4,10 +4,70 @@
 
 //color changer
 const div1 = document.createElement("div1");
-div1.style.width = "100px";
-div1.style.height = "100px";
-div1.style.background = "red";
-div1.style.color = "white"; //presence of font needed due to three squares looking like a single rectangle
-div1.innerHTML = "One__"; //has underscores to add space to text
-document.body.appendChild(div1); //creates an end for parent node
-div1.addEventListener("click", changeStyles);
+div1.style.width = "200px";
+div1.style.height = "200px";
+div1.style.marginLeft = "5px";
+div1.style.background = "grey";
+div1.style.color = "white";
+div1.innerHTML = "Red";
+document.body.appendChild(div1);
+div1.addEventListener("click", changeColor);
+
+const div2 = document.createElement("div2");
+div2.style.width = "200px";
+div2.style.height = "200px";
+div2.style.marginLeft = "5px";
+div2.style.background = "grey";
+div2.style.color = "white";
+div2.innerHTML = "Green";
+document.body.appendChild(div2);
+div2.addEventListener("click", changeColor);
+
+const div3 = document.createElement("div3");
+div3.style.width = "200px";
+div3.style.height = "200px";
+div3.style.marginLeft = "5px";
+div3.style.background = "grey";
+div3.style.color = "white";
+div3.innerHTML = "Blue";
+document.body.appendChild(div3);
+div3.addEventListener("click", changeColor);
+
+function changeColor(){
+    let change = div1.style.background = "red";
+    event.target.innerHTML = change;
+}
+
+function changeColor2(){
+    let change = div2.style.background = "green";
+    event.target.innerHTML = change;
+}
+
+function changeColor3(){
+    let change = div3.style.background = "blue";
+    event.target.innerHTML = change;
+}
+
+//Color Mixer
+
+//Flash Cards
+let quest1 = document.getElementById("question1");
+quest1.addEventListener("click", answer1);
+
+let quest2 = document.getElementById("question2");
+quest2.addEventListener("click", answer2);
+
+let quest3 = document.getElementById("question3");
+quest3.addEventListener("click", answer3);
+
+function answer1() {
+    event.target.innerHTML = "Indianopolis";
+}
+
+function answer2() {
+    event.target.innerHTML = "Metal";
+}
+
+function answer3() {
+    event.target.innerHTML = "Chicken";
+}
